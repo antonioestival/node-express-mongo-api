6e2bc45972c969cccfa708a3d99e7ec38889cfdc
unknown <antonioestival@gmail.com>
2019-04-14 00:38:59 -0300

    heroku, alteração de varivel default de ambiente para server port

--- src/config/index.js
+++ src/config/index.js
@@ -30,8 +30,8 @@ export default {
 		secret: process.env.JWT_SECRET || 'murilosecret',
 	},
 	server: {
-		port: process.env.SERVER_PORT || 3000,
-		host: process.env.SERVER_HOST || '127.0.0.1',
+		port: process.env.PORT || 3000,
+		host: process.env.HOST || '127.0.0.1',
 	},
 	database: {
 		uri: process.env.MONGODB_URI || 'mongodb://localhost:27017/astronauta',
